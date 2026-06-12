225e279224ff2c4292e9c25e3b4d5c7fa4798cc0
Mutasim Fouad Showmik <124583421+Fouad-Showmik@users.noreply.github.com>
2026-06-12 14:29:28 +0600

    Add basic DateTime usage in using_DateTime_class.cs

---
+++ using_DateTime_class.cs
@@ -0,0 +1,12 @@
+namespace BasicFirst
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.UtcNow;
+            Console.WriteLine(dateTime);
+        }
+    }
+}
